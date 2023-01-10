533d6a98bc040b0740bc5773532fa4268664df7d
khaled-m1 <alanziq00@gmail.com>
2023-01-10 17:45:37 +0300

    sanity/studio :camel:

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,16 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
-export default defineConfig({
-  name: "default",
-  title: "blog-nextjs",
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
-  projectId: "5ie0qx7x",
-  dataset: "production",
+export default defineConfig({
+  basePath: "/studio",
+  name: "KMA_Contact_Managment",
+  title: "KMA Contact Managment Title",
+
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
